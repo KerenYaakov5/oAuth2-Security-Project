7f100a7661bb0371b442c3665d2cd772beb450fb
KerenyShenkar <kereny@shenkar.ac.il>
2021-07-07 00:38:53 +0300

    Added main tafg to pages

--- App/scripts.js
+++ App/scripts.js
@@ -6,7 +6,13 @@ function sendForm() {
     $("#oAuthForm button").click((event) => {
         event.preventDefault();
 
-        console.log("oAuthForm");
-        // Send to server
+        const clientCredentials = $("#clientAuthentication").val();
+
+        if (!clientCredentials) {
+            console.log("Error: client credentials value is empty");
+        } else {
+            console.log("oAuthForm: " + clientCredentials);
+            // Send to server
+        }
     });
 }
