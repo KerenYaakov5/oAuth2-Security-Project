e861e0ed26f6b59122517e9c3d3ec46076bd296d
KerenyShenkar <kereny@shenkar.ac.il>
2021-07-08 09:21:28 +0300

    authorization server - db connection to Mongo

--- Authorization Server/index.js
+++ Authorization Server/index.js
@@ -1,26 +1,3 @@
-const express = require("express");
-const app = express();
-const port = process.env.AUTH_SERVER_PORT || 3000;
-
-const { oAuthRouter } = require("./Routers/oAuthRouter");
-const { userRouter } = require("./Routers/userRouter");
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.set('Content-Type', 'application/json');
-    next();
-});
-
-app.use('/api/oauth2', oAuthRouter);
-app.use('/api/user', userRouter);
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something is broken!');
-});
-
-app.listen(port, () => console.log(`Authorization server is running on port ${port}`));
+require('dotenv').config();
+require('./server');
+require('./dbConnection');
