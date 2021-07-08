2c62e74436005618dcf53ef4cd1fc9a5508ef6d2
KerenyShenkar <kereny@shenkar.ac.il>
2021-07-08 17:21:54 +0300

    login functionality - client side and cookies

--- App/js/connectedUser.js
+++ App/js/connectedUser.js
@@ -1,21 +1,15 @@
 $(document).ready(() => {
-    checkConnectedUser();
+    const isConnected = isUserConnected();
+
+    if (!isConnected) {
+        window.location.href = "../settings/login.html";
+    } else {
+        window.location.href = "../info/oauth2.html";
+    }
 });
 
-function checkConnectedUser() {
+function isUserConnected() {
     const cookie = document.cookie;
 
-    if (cookie) {
-        console.log("Cookie is: " + cookie);
-
-        // get the client id and validate 
-    } else {
-        console.log("No cookie");
-
-        window.location.href = "../settings/login.html";
-    }
-
-    // const now = new Date();
-    // now.setTime(now.getTime() + (60*60*1000)); // For an hour
-    // document.cookie = `userid=2611, username=keren, expires=${now}`;
+    return !!cookie;
 }
--- App/js/login.js
+++ App/js/login.js
@@ -1,5 +1,11 @@
 $(document).ready(() => {
-    addLoginFormEventListener();
+    const isConnected = isUserConnected();
+
+    if (!isConnected) {
+        addLoginFormEventListener();
+    } else {
+        window.location.href = "../info/oauth2.html";
+    }
 });
 
 function addLoginFormEventListener() {
@@ -18,6 +24,38 @@ function addLoginFormEventListener() {
 }
 
 function sendLoginRequest(username, password) {
-    // ajax
+    const loginRequestData = {
+        username: username,
+        password: password
+    }
+
+    $.ajax({
+        url: "http://localhost:3000/api/login", // TODO - change to real server 
+        method: "POST",
+        data: loginRequestData,
+        cache: false,
+        success: (response) => {
+            if (response && response.userId) {
+                saveUserInCookie(response.userId, username);
+                window.location.href = "../info/oauth2.html";
+            }
+        },
+        error: (error) => {
+            console.log(`Error on login request: ${error}`);
+        }
+    });
 }
 
+function saveUserInCookie(userId, userName) {
+    const now = new Date();
+    now.setTime(now.getTime() + (60*60*1000)); /* Save for an hour */
+
+    document.cookie = `userId=${userId}; expires=${now}; Path=/`;
+    document.cookie = `userName=${userName}; expires=${now}; Path=/`;
+}
+
+function isUserConnected() {
+    const cookie = document.cookie;
+
+    return !!cookie;
+}
