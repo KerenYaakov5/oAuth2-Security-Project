fc986dce641ef5105567ea2fbef25244edcd3b57
KerenyShenkar <kereny@shenkar.ac.il>
2021-07-25 20:48:54 +0300

    Removed 'resource server' folder

--- Resource Server/Controllers/employeesController.js
+++
@@ -1,6 +0,0 @@
-exports.employeesController = {
-    getEmployees(req, res) {
-        console.log('employeesController getEmployees');
-        res.json("employees json data");
-    }
-};
--- Resource Server/Routers/employeesRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const { employeesController } = require('../Controllers/employeesController');
-
-const employeesRouter = new Router();
-
-employeesRouter.get('/', employeesController.getEmployees);
-
-module.exports = { employeesRouter };
